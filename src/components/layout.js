19201392be553cec99d0f70824cda5deaeb85aac
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-06-02 12:31:50 -0700

    adjusted styles.

--- src/components/layout.js
+++ src/components/layout.js
@@ -21,9 +21,7 @@ import weddingJpg from '../images/wedding-bg.jpg';
 import receptionJpg from '../images/reception-bg.jpg';
 
 
-const Background = styled.div.attrs(({ url }) => ({
-  url: url
-}))`
+const Background = styled.div`
   position: relative;
 
   ::before {
@@ -31,10 +29,10 @@ const Background = styled.div.attrs(({ url }) => ({
     position: absolute;
     top: 0; left: 0;
     width: 100%; height: 100%;
-    background-color: black;
+    background-color: whitesmoke;
     background-image: url(${props => props.url});
     background-repeat: no-repeat;
-    background-attachment: fixed;
+    ${props => props.safari ? null : `background-attachment: fixed;`}
     background-position: center top;
     background-size: 100% 100vh;
     filter: brightness(70%);
@@ -46,12 +44,12 @@ const Background = styled.div.attrs(({ url }) => ({
 `
 
 const Main = styled.main`
-  min-height: calc(100vh - 56px);
+  min-height: calc(100vh - 112px);
   margin: 0 auto;
   padding: 1rem;
 
   @media (max-width: 1024px) {
-    max-height: calc(100vh - 56px);
+    max-height: calc(100vh - 112px);
   }
 `
 
@@ -84,7 +82,7 @@ const Layout = ({ children }) => {
   }
 
   return(
-    <Background url={url}>
+    <Background url={url} safari={safari}>
       <div className="relative">
         <Header />
         <Main className="lg:w-1/2 w-full relative overflow-scroll scrolling-touch lg:overflow-visible">{children}</Main>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from 'gatsby'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -6,12 +7,15 @@ import Videos from "../components/videos"
 
 const heading = 'lg:text-7xl lg:h-large h-auto my-12 text-5xl inline-flex flex-col justify-between items-center lg:mt-16 leading-none'
 
+const fab = "rounded-full border border-black bg-white text-black hover:text-black text-2xl text-center leading-snug font-bold fixed bottom-1/10 right-1/10 w-12 h-12 p-2"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <p className="text-red-600 font-sans lg:hidden block">[Penny House Weddings]</p>
-    <h1 className={heading}><span>PENNY</span><span>HOUSE</span><span className="font-sans lg:text-5xl text-red-600">[Weddings]</span></h1>
+    <h1 className={heading} id="company"><span>PENNY</span><span>HOUSE</span><span className="font-sans lg:text-5xl text-red-600">[Weddings]</span></h1>
     <Videos />
+    <Link to="#company" id="fab" activeClassName="active" className={fab}>^</Link>
   </Layout>
 )
 
